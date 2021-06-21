8e7a2dbfcbd554a622c37db92b7ea82ae1f05230
Alberto Minoru <alberto.minoru@gmail.com>
2021-06-21 13:48:22 -0300

    Initial commit

---
+++ posts/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+
+
+class BlogTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(username='test_user', password='abc123')
+        test_user.save()
+
+        test_post = Post.objects.create(author=test_user, title='Blog title', body='Body content...')
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'test_user')
+        self.assertEqual(title, 'Blog title')
+        self.assertEqual(body, 'Body content...')
